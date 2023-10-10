618f8cfff8cb8f3e9c5c1a472abcaafaf9d38e76
Steven <141743202+StxvxnsGithub@users.noreply.github.com>
2023-10-10 14:45:05 +0100

    Commenting

--- src/utils/getCommands.js
+++ src/utils/getCommands.js
@@ -1,12 +1,12 @@
-const fs = require("node:fs");
-const path = require("node:path");
+const fs = require("node:fs"); // Module for using the file system
+const path = require("node:path"); // Module for working with file paths
 
-module.exports = () => {
-    const commands = [];
-    const commandsFolderPath = path.join(__dirname, "..", "commands");
-    const commandFolders = fs.readdirSync(commandsFolderPath);
+module.exports = () => { // Exports the following code for it to be used externally
+    const commands = []; // Creates an array that will hold individual command files
+    const commandsFolderPath = path.join(__dirname, "..", "commands"); // Defines the path to find commands (./../commands)
+    const commandFolders = fs.readdirSync(commandsFolderPath); // Reads the folder containing individual command folders/categories
 
-    for (const commandFolder of commandFolders) {
+    for (const commandFolder of commandFolders) { // Loops through the commands
         const commandPath = path.join(commandsFolderPath, commandFolder);
         const commandFiles = fs
             .readdirSync(commandPath)
