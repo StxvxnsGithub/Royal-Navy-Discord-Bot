761af6aa3fa623609a9b0b9ef408985843651138
Steven <141743202+StxvxnsGithub@users.noreply.github.com>
2023-10-10 15:48:45 +0100

    Commenting

--- src/utils/splitFieldLines.js
+++ src/utils/splitFieldLines.js
@@ -1,16 +1,16 @@
-module.exports = (fieldTitle, fieldText) => {
-    try {
-        const fieldTextLines = fieldText.split("\\n");
-        const embedFields = [];
+module.exports = (fieldTitle, fieldText) => { // // Exports the following code for it to be used externally
+    try { // Attempts to create multi-line paragraphed field
+        const fieldTextLines = fieldText.split("\\n"); // The \ before \n means literal \n is recognised
+        const embedFields = []; // Fields for the embed message
 
-        embedFields.push({ name: fieldTitle, value: fieldTextLines[0] });
-        for (let i = 1; i < fieldTextLines.length; i++) {
-            embedFields.push({ name: " ", value: "\n" });
-            embedFields.push({ name: " ", value: fieldTextLines[i] });
+        embedFields.push({ name: fieldTitle, value: fieldTextLines[0] }); // Creates the first paragraph
+        for (let i = 1; i < fieldTextLines.length; i++) { // Iterates through the remaining paragraphs in the field
+            embedFields.push({ name: " ", value: "\n" }); // Adds a field with an empty field title and field text to replicate a new line
+            embedFields.push({ name: " ", value: fieldTextLines[i] }); // Adds a paragraph
         }
 
-        return embedFields;
-    } catch (error) {
+        return embedFields; // Returns the fields split into different paragraphs
+    } catch (error) { // Catches and reports any errors
         console.error(`\nSplit Field Lines ERROR: \n${error}\n`);
         return;
     }
