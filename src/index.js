8ab1a23a2de8ecd49748722946e58841a2348eeb
Steven <141743202+StxvxnsGithub@users.noreply.github.com>
2023-10-14 13:01:20 +0100

    Commenting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const getCommands = require("./utils/getCommands");
+const fs = require("node:fs"); // Module for using the file system
+const path = require("node:path"); // Module for working with file paths
+const getCommands = require("./utils/getCommands"); // Imports the getCommands function
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js"); // Requires the necessary discord.js classes
 const { token } = require("../config.json"); // Requires the config.json file storing important data
 
-const client = new Client({
+const client = new Client({ // Initialises an instance of the client class, enabling intents
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -12,25 +12,27 @@ const client = new Client({
     ],
 });
 
-client.commands = new Collection();
+client.commands = new Collection(); // Creates a collection to store the bot's commands
 
-getCommands().forEach((command) => {
-    client.commands.set(command.data.name, command);
+getCommands().forEach((command) => { // Retrieves each command
+    client.commands.set(command.data.name, command); // Adds each command to the commands collection
 });
 
-const eventsFolderPath = path.join(__dirname, "events");
+const eventsFolderPath = path.join(__dirname, "events"); // Creates a path to the events folder
 const eventFiles = fs
-    .readdirSync(eventsFolderPath)
-    .filter((file) => file.endsWith(".js"));
+    .readdirSync(eventsFolderPath) // Reads files in the events folder
+    .filter((file) => file.endsWith(".js")); // Only obtains the .js files
 
-for (const eventFile of eventFiles) {
-    const filePath = path.join(eventsFolderPath, eventFile);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
+for (const eventFile of eventFiles) { // Loops through the files of events
+    const filePath = path.join(eventsFolderPath, eventFile); // Creates a path to the event file
+    const event = require(filePath); // Imports the function within the event file
+
+    // Checks if the event should only be ran once
+    if (event.once) { 
+        client.once(event.name, (...args) => event.execute(...args)); // If one-time event, creates a one-time listener
     } else {
-        client.on(event.name, (...args) => event.execute(...args));
+        client.on(event.name, (...args) => event.execute(...args)); // Else, creates a standard event listener
     }
 }
 
-client.login(token);
+client.login(token); // Logs into and starts the bot
