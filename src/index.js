44635f94e158a5bfe3027f38c050b00e17eda5c1
Steven <141743202+StxvxnsGithub@users.noreply.github.com>
2023-10-19 23:00:21 +0100

    Commenting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const getCommands = require("./utils/getCommands"); // Imports the getCommands f
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js"); // Requires the necessary discord.js classes
 const { token } = require("../config.json"); // Requires the config.json file storing important data
 
-const client = new Client({ // Initialises an instance of the client class, enabling intents
+const client = new Client({
+    // Initialises an instance of the client class, enabling intents
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -14,7 +15,8 @@ const client = new Client({ // Initialises an instance of the client class, enab
 
 client.commands = new Collection(); // Creates a collection to store the bot's commands
 
-getCommands().forEach((command) => { // Retrieves each command
+getCommands().forEach((command) => {
+    // Retrieves each command
     client.commands.set(command.data.name, command); // Adds each command to the commands collection
 });
 
@@ -23,12 +25,13 @@ const eventFiles = fs
     .readdirSync(eventsFolderPath) // Reads files in the events folder
     .filter((file) => file.endsWith(".js")); // Only obtains the .js files
 
-for (const eventFile of eventFiles) { // Loops through the files of events
+for (const eventFile of eventFiles) {
+    // Loops through the files of events
     const filePath = path.join(eventsFolderPath, eventFile); // Creates a path to the event file
     const event = require(filePath); // Imports the function within the event file
 
     // Checks if the event should only be ran once
-    if (event.once) { 
+    if (event.once) {
         client.once(event.name, (...args) => event.execute(...args)); // If one-time event, creates a one-time listener
     } else {
         client.on(event.name, (...args) => event.execute(...args)); // Else, creates a standard event listener
